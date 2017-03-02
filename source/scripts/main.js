1215d6ba8e8d46c0259d9212837dbcdec05b53c2
Anthony2511 <anthonybeaumecker@gmail.com>
2017-03-02 14:53:12 +0100

    Add menu (desktop and responsive) + header + section biography

---
+++ source/scripts/main.js
@@ -0,0 +1,20 @@
+/*
+Responsive nav menu with sub menus only on larger screens.
+
+This is more than three years old now - please take it with a big pinch of salt - there are better ways to achieve the same thing!
+*/
+
+/**********
+MOBILE MENU
+**********/
+$('.navigation__toggle').click(function(e){
+  //click event for left clicks only! http://www.jacklmoore.com/notes/click-events
+  if (!(e.which > 1 || e.shiftKey || e.altKey || e.metaKey)) {
+    e.preventDefault();
+    if($(this).parent().find('.navigation__menu').hasClass('expanded-mobile-menu')){
+      $(this).removeClass('expanded-menu-toggle').parent().removeClass('nav-expanded').find('.navigation__menu').removeClass('expanded-mobile-menu');
+    }else{
+      $(this).addClass('.navigation__expanded-toggle').parent().addClass('nav-expanded').find('.navigation__menu').addClass('expanded-mobile-menu');
+    }
+  }
+});
