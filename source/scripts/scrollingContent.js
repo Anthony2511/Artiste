fa57d572aec57375cb87c98b3e9cd3df030b979a
Anthony2511 <anthonybeaumecker@gmail.com>
2017-03-14 23:09:55 +0100

    add animations menu contenu deroulant

---
+++ source/scripts/scrollingContent.js
@@ -0,0 +1,5 @@
+document.getElementById('link').addEventListener('click', function (e) {
+    e.preventDefault();
+  // Using an if statement to check the class
+  document.getElementById("contenu").classList.toggle('defil');
+});
